155b0bc02c9ed77a57b7ebd6fb691b471c7793ea
minjunj <minjun_jo@gm.gist.ac.kr>
2025-05-05 16:29:52 +0900

    edit operation

--- operation/setup.sh
+++ operation/setup.sh
@@ -23,6 +23,12 @@ echo "
 # Powerlevel10k 설치
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
 echo "
 ######################
 #symlink create phase#
